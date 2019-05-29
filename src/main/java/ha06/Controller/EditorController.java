0f067cf5867563f7d66afcda3364ab9d69ec7c1e
vakanto <vakanto@posteo.eu>
2019-05-29 12:48:40 +0200

    Added text processing

--- src/main/java/ha06/Controller/EditorController.java
+++ src/main/java/ha06/Controller/EditorController.java
@@ -1,10 +1,14 @@
 package ha06.Controller;
 
+import ha06.Handler.CommandLineHandler;
 import ha06.Model.EditorModel;
 import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 
@@ -20,12 +24,25 @@ public class EditorController implements Initializable {
     private Pane sheet;
 
     private EditorModel editorModel;
-
+    private CommandLineHandler commandLineHandler;
     public EditorController(){
+        commandLineHandler=new CommandLineHandler();
 
     }
 
     public EditorController(EditorModel editorModel){
+        commandLineHandler=new CommandLineHandler();
+    }
+
+    @FXML
+    public void processCommand(KeyEvent keyEvent){
+        if(keyEvent.getCode()!= KeyCode.ENTER){
+            System.out.println("Wrong Button pressed");
+            return;
+        }
+        TextField textField = (TextField) keyEvent.getSource();
+        String command = textField.getText();
+        System.out.println(command);
     }
 
     public void drawLine(){
